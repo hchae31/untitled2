700970f2be69cbce6224f8da91edb9f3c1834bbd
hchae31 <chaeheegon5858@gmail.com>
2024-08-27 14:20:15 +0900

    sqlalchemy orm을 이용해서 1:n관계설정

--- app/model/gallery.py
+++ app/model/gallery.py
@@ -1,8 +1,9 @@
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy import ForeignKey
 from datetime import datetime
 from app.model.base import Base
 
+# back_populates : 양방향 관계설정, 관계의 상호참조
 class Gallery(Base):
     __tablename__ = 'gallery'
 
@@ -12,6 +13,8 @@ class Gallery(Base):
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str]
+    attachs = relationship('GalAttach', back_populates='gallery') # 하나의 Gallery는 하나 이상의 Attach가 존재 (1:n)
+
 
 class GalAttach(Base):
     __tablename__ = 'galattach'
@@ -21,3 +24,4 @@ class GalAttach(Base):
     fname: Mapped[str] = mapped_column(nullable=False)
     fsize: Mapped[int] = mapped_column(default=0)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    gallery = relationship('Gallery', back_populates='attachs') # 하나의 attach는 하나의 gallery에 속함
