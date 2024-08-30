fe4fd1b86e1b1fcfafb7ff0734f4926168e30877
hchae31 <chaeheegon5858@gmail.com>
2024-08-30 13:44:45 +0900

    sqlite: regdate 마이크로 초를 0으로 설정

--- app/model/board.py
+++ app/model/board.py
@@ -1,7 +1,8 @@
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import ForeignKey
 from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.model.base import Base
+
 
 class Board(Base):
     __tablename__ = 'board'
@@ -12,3 +13,31 @@ class Board(Base):
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str]
+    replys = relationship('Reply', back_populates='board')
+
+
+
+class Reply(Base):
+    __tablename__ = 'reply'
+    rno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    reply: Mapped[str] = mapped_column(index=True)
+    userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True)
+    # regdate: Mapped[datetime] = mapped_column(default=datetime.now)
+    regdate: Mapped[datetime] = mapped_column(default=lambda: datetime.now().replace(microsecond=0))
+    bno: Mapped[int] = mapped_column(ForeignKey('board.bno'))
+    rpno: Mapped[int] = mapped_column(ForeignKey('reply.bno'))
+    board = relationship('Board', back_populates='replys')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
