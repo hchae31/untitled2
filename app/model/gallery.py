2e2b38ef65844d4c61fbe21c9e7288d6e04cf0bd
hchae31 <chaeheegon5858@gmail.com>
2024-09-03 10:43:52 +0900

    sqlite모델을 mariadb로 마이그레이션

--- app/model/gallery.py
+++ app/model/gallery.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import ForeignKey
 from datetime import datetime
+
+from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.model.base import Base
 
 # back_populates : 양방향 관계설정, 관계의 상호참조
@@ -8,20 +10,28 @@ class Gallery(Base):
     __tablename__ = 'gallery'
 
     gno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
-    title: Mapped[str] = mapped_column(index=True)
-    userid: Mapped[str] = mapped_column(ForeignKey('member.userid'), index=True)
+    title: Mapped[str] = mapped_column(String(100),index=True)
+    userid: Mapped[str] = mapped_column(String(10), ForeignKey('member.userid'), index=True)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
-    contents: Mapped[str]
-    attachs = relationship('GalAttach', back_populates='gallery') # 하나의 Gallery는 하나 이상의 Attach가 존재 (1:n)
-
+    contents: Mapped[str] = mapped_column(Text)
+    attachs = relationship('GalAttach', back_populates='gallery')  # 하나의 gallery는 하나 이상의 attach 존재 (1:n)
 
 class GalAttach(Base):
     __tablename__ = 'galattach'
 
     gano: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     gno: Mapped[int] = mapped_column(ForeignKey('gallery.gno'), index=True)
-    fname: Mapped[str] = mapped_column(nullable=False)
+    fname: Mapped[str] = mapped_column(String(35), nullable=False)
     fsize: Mapped[int] = mapped_column(default=0)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
-    gallery = relationship('Gallery', back_populates='attachs') # 하나의 attach는 하나의 gallery에 속함
+    gallery = relationship('Gallery', back_populates='attachs')  # 하나의 attach는 하나의 gallery에 속함 (1:1)
+
+
+
+
+
+
+
+
+
